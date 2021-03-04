199e6870e936e7442e49e784d9182eea2471515d
MiniKrishna <mack.romonenko@gmail.com>
2021-03-04 14:23:30 +0300

    получение координат

--- Lab1/agent.js
+++ Lab1/agent.js
@@ -1,45 +1,7 @@
 const Msg = require('./msg') // Подключение модуля разбора сообщений от сервера
 const readline = require('readline') // Подключение модуля ввода из командной строки
+const Vision = require('./vision') // Подключения модуля зрения
 
-const Flags = {
-	ftl50: {x: -50, y: 39}, ftl40: {x: -40, y: 39},
-	ftl30: {x: -30, y: 39}, ftl20: {x: -20, y: 39},
-	ftl10: {x: -10, y: 39}, ft0: {x: 0, y: 39},
-	ftr10: {x: 10, y: 39}, ftr20: {x: 20, y: 39},
-	ftr30: {x: 30, y: 39}, ftr40: {x: 40, y: 39},
-	ftr50: {x: 50, y: 39}, fbl50: {x: -50, y: -39},
-	fbl40: {x: -40, y: -39}, fbl30: {x: -30, y: -39},
-	fbl20: {x: -20, y: -39}, fbl10: {x: -10, y: -39},
-	fb0: {x: 0, y: -39}, fbr10: {x: 10, y: -39},
-	fbr20: {x: 20, y: -39}, fbr30: {x: 30, y: -39},
-	fbr40: {x: 40, y: -39}, fbr50: {x: 50, y: -39},
-	flt30: {x:-57.5, y: 30}, flt20: {x:-57.5, y: 20},
-	flt10: {x:-57.5, y: 10}, fl0: {x:-57.5, y: 0},
-	flb10: {x:-57.5, y: -10}, flb20: {x:-57.5, y: -20},
-	flb30: {x:-57.5, y: -30}, frt30: {x: 57.5, y: 30},
-	frt20: {x: 57.5, y: 20}, frt10: {x: 57.5, y: 10},
-	fr0: {x: 57.5, y: 0}, frb10: {x: 57.5, y: -10},
-	frb20: {x: 57.5, y: -20}, frb30: {x: 57.5, y: -30},
-	fglt: {x:-52.5, y: 7.01}, fglb: {x:-52.5, y:-7.01},
-	gl: {x:-52.5, y: 0}, gr: {x: 52.5, y: 0}, fc: {x: 0, y: 0},
-	fplt: {x: -36, y: 20.15}, fplc: {x: -36, y: 0},
-	fplb: {x: -36, y:-20.15}, fgrt: {x: 52.5, y: 7.01},
-	fgrb: {x: 52.5, y:-7.01}, fprt: {x: 36, y: 20.15},
-	fprc: {x: 36, y: 0}, fprb: {x: 36, y:-20.15},
-	flt: {x:-52.5, y: 34}, fct: {x: 0, y: 34},
-	frt: {x: 52.5, y: 34}, flb: {x:-52.5, y: -34},
-	fcb: {x: 0, y: -34}, frb: {x: 52.5, y: -34},
-	
-	distance(f1, f2) {
-	    return Math.sqrt((f1.x-f2.x)**2+(f1.y-f2.y)**2)
-	},
-	
-	contains(f) {
-	    if (typeof this.f !== 'undefined')
-	        return true;
-	    return false;
-	},
-}
 
 class Agent {
     constructor() {
@@ -105,14 +67,8 @@ class Agent {
     // msg - text message with all information
     // cmdType - type of command
     // gameObjects - array, where type of elemement is [[params], [objectName]]. ObjectName is array of literals
-	console.log(gameObjects);
 	if (cmdType === "see") {
-	    let flagArray = [];
-	    for (let element of gameObjects) {
-	    	if (Flags.contains(element.cmd.p.join('')))
-	    	   console.log(true);
-	    }
-	    
+	    Vision.calculatePos(gameObjects);
 	    // ТЗ
 	    // 1) ВыЧЛЕНить все существующие флаги
 	    // 2) В зависимости от их количества применить алгоритм вычисления координат игрока
