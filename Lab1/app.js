12f4c812d715c7630e8b60356e5465df83e83cc6
MiniKrishna <mack.romonenko@gmail.com>
2021-03-05 10:06:17 +0300

    фикс

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,9 +1,11 @@
 const Agent = require('./agent') // Импорт агента
 const VERSION = 7 // Версия сервера
+
 let myAgent = new Agent(); // Создание экземпляра агента
-myAgent.readParam();
 require('./socket')(myAgent, "teamA", VERSION) //Настройка сокета
-let enemyAgent1 = new Agent(); // Создание экземпляра агента
-require('./socket')(enemyAgent1, "teamB", VERSION) //Настройка сокета
-enemyAgent1.socketSend("move",`-10 -15`);
+myAgent.readParam();
+let enemyAgent = new Agent(); // Создание экземпляра агента
+require('./socket')(enemyAgent, "teamB", VERSION) //Настройка сокета
+setTimeout(() => enemyAgent.socketSend("move",`-8 -15`), 1000);
+
 
